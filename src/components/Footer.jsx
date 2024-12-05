75cc4e3f65d43b4ad7f9dc0cf00310a005b042e0
Owen <owenouiluobe@gmail.com>
2024-12-05 10:44:52 +0100

    logo mealsDB added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer>
-      <a href="http://localhost:5173"></a>
-      <p>© 2024 Owen Iluobe - All rights reserved</p>
+        <a href="http://localhost:5173"><img className="logo-meals" src="https://www.themealdb.com/images/logo.png" alt="Logo de meal" /></a>
+        <p>© 2024 Owen Iluobe - All rights reserved</p>
       <p>Made with <span role="img" aria-label="love">❤️</span> by <a href="https://github.com/Owenews">Owen Iluobe</a></p>
       <div id="scrollUp">
       <a href="#top" className="scroll-btn">
