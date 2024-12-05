4f7b729ebc219a09a0d121950bdb8f4f4afdb47c
Owen <owenouiluobe@gmail.com>
2024-12-05 10:09:22 +0100

    Style added to the reaserch bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+
 const Header = () => {
-  return (
-    <header>
+    const navigate = useNavigate();
+
+    const handleSubmitSearch = (event) => {
+      event.preventDefault();
+      const query = event.target.query.value;
+  
+      navigate("/search-results?query=" + query);
+    };
+
+    return (
+      <header>
+        <h1>Les recettes de Roberto</h1>
+
         <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/meals">Meals</Link>
-                </li>
-            </ul>
+          <ul>
+            <li>
+              <Link to={"/"}>Accueil</Link>
+            </li>
+
+            <li>
+              <Link to={"/contact"}>Contact</Link>
+            </li>
+
+            <li>
+              <Link to={"/meals"}>Toutes les recettes</Link>
+            </li>
+          </ul>
+
+          <form method="get" onSubmit={handleSubmitSearch}>
+            <label>
+               <input type="search" name="query" placeholder="Search for a meal" />
+            </label>
+
+            <input type="submit" />
+          </form>
         </nav>
-    </header>
-    
-  );
+      </header>
+    );
 };
 
 export default Header;
