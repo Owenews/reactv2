f072b2607e5ad5b633cc0bbf5d71db872b70b284
Owen <owenouiluobe@gmail.com>
2024-12-05 10:03:20 +0100

    comments added

--- src/page/RandomMealsPage.jsx
+++ src/page/RandomMealsPage.jsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const RandomMealsPage = () => {
     const [randomMeal, setRandomMeal] = useState(null);
 
-    // Fetch cocktail function
+    // Fetch meal function
     const fetchCocktail = async () => {
       const randomMealResponse = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
       const randomMealData = await randomMealResponse.json();
       setRandomMeal(randomMealData.meals[0]);
     };
   
-    // useEffect to fetch a random cocktail on component mount
+    // useEffect to fetch a random meal on component mount
     useEffect(() => {
       fetchCocktail();
     }, []); // Empty dependency array means it runs only once, when the component mounts
   
     const handleChangeRandomMeal = () => {
-      fetchCocktail(); // Fetch a new random cocktail when the button is clicked
+      fetchCocktail(); // Fetch a new random meal when the button is clicked
     };
 
 
@@ -25,27 +26,12 @@ const RandomMealsPage = () => {
     <section>        
         <h2>Random cocktail</h2>
         <button className ="drink-button" onClick={handleChangeRandomMeal}>Change meal</button>
-        {/* Afficher les résultats si ils existent */}
+        {/* Display results if available */}
         {randomMeal ? (
             <article className="meal-card" key={randomMeal.strMeal}>
                 <h2><em>{randomMeal.strMeal}</em></h2>
                 <img className="meal-image" src={randomMeal.strMealThumb} alt={randomMeal.strMeal} />
-                <table className="meal-info">
-                  <tbody>
-                    <tr>
-                      <td><strong>Instructions:</strong></td>
-                      <td>{randomMeal.strInstructions}</td>
-                    </tr>
-                    <tr>
-                      <td><strong>Ingrédients:</strong></td>
-                      <td>{randomMeal.strIngredient1}</td>
-                    </tr>
-                    <tr>
-                      <td><strong>Mesures:</strong></td>
-                      <td>{randomMeal.strMeasure1}</td>
-                    </tr>
-                  </tbody>
-                </table>
+                <Link to={`/meals/${randomMeal.idMeal}`}><button className="meal-list-button">See more</button></Link>
             </article>
         ) : (
           <p>En cours de chargement...</p>
